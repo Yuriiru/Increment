9a84dcad8ed61cf1aad4c18bb5c899029c7935a5
Yuriiru <98612792+Yuriiru@users.noreply.github.com>
2022-11-17 12:05:49 +0300

    Add files via upload

---
+++ SqClient.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+public class SqClient {
+
+    public static void main(String[] args) throws IOException {
+        int counter = 0;
+        try(Socket socket = new Socket("MacBook-Air-Yurii.local", 1020)){
+            counter++;
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write(counter);
+            outputStream.flush();
+
+            InputStream inputStream = socket.getInputStream();
+
+            while (counter < 10) {
+                int response = inputStream.read();
+                System.out.println(response);
+            }
+        }
+
+    }
+}
